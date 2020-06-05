6a72110973af97ca02a0786014d5321df938d4f2
Alex <sagaidak1337@gmail.com>
2020-06-05 07:41:38 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+// @ts-nocheck
+import React, { useEffect, useState } from 'react';
+import { getData } from './data'
 import './App.css';
 
 function App() {
+  const [ data, setData ] = useState({})
+
+  useEffect(() => {
+    getData().then(res => {
+      setData(res)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <h3>Rates:</h3>
+        {
+          data && data.rates && Object.entries(data.rates).map(([name, value]) => {
+            return <div className="row" key={name}>
+              <div className="left">{name}</div>
+              <div className="right">{value}</div>
+            </div>
+          })
+        }
+      </div>
     </div>
   );
 }
