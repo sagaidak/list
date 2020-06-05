21cfb697363895106b382caffa56acafc3890e8e
Alex <sagaidak1337@gmail.com>
2020-06-05 07:44:34 +0300

    Updates

---
+++ precache-manifest.b85018950400de18664108a1ad377558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2fe7192514d1f8d6227e0468acbc8d",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "9300ec2cf545497a18ac",
+    "url": "/list/static/css/main.f84f0a15.chunk.css"
+  },
+  {
+    "revision": "c31b3a5214aa6a1a7673",
+    "url": "/list/static/js/2.64ce18bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list/static/js/2.64ce18bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9300ec2cf545497a18ac",
+    "url": "/list/static/js/main.43ce442f.chunk.js"
+  },
+  {
+    "revision": "051cb6f8a953421a46ed",
+    "url": "/list/static/js/runtime-main.526a269b.js"
+  }
+]);
